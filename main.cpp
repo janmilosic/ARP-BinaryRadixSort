4724d70ad316c2f087d5ec83a560f7262da98c06
janmilosic <milosic.jan@gmail.com>
2025-03-29 18:39:42 +0100

    added function to read input from a file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+bool Branje_Stevil(vector<int> &vec, const char s[]) {
+    ifstream input(s);
+    int st;
+
+    if (!input.is_open()) {
+        return false;
+    }
+
+    while (!input.eof()) {
+        input >> st;
+        vec.push_back(st);
+        while (isspace(input.peek())) input.get();
+    }
+    input.close();
+    return true;
+}
 
 int main() {
     std::cout << "Binary Radix sort";
